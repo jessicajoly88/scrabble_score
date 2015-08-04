370ca008f4fbc798a761b48f0efcb5cf5d8c3fcc
Jessica Joly <jessicajoly88@gmail.com>
2015-08-04 15:02:28 -0700

    Final commit using class method instead of defining function

--- lib/scrabble_score.rb
+++ lib/scrabble_score.rb
@@ -1,11 +1,13 @@
-define_method (:scrabble_score) do |word|
-  word.downcase!()
-  score = 0
-  new_array=word.split("")
-  letter_hash = {'a'=> 1,'e'=>1,'i'=>1,'o'=>1,'u'=>1,'l'=>1,'n'=>1,'r'=>1,'s'=>1,'t'=>1,'d'=>2,'g'=>2,'b'=>3,'c'=>3,'m'=>3,
-    'p'=>3,'f'=>4,'h'=>4,'v'=>4,'w'=>4,'y'=>4,'k'=>5,'j'=>8,'x'=>8,'q'=>10,'z'=>10}
-    new_array.each() do |letter|
-      score+=letter_hash.fetch(letter)
-    end
-    score 
+class String
+  define_method (:scrabble_score) do
+    self.downcase!()
+    score = 0
+    new_array=self.split("")
+    letter_hash = {'a'=> 1,'e'=>1,'i'=>1,'o'=>1,'u'=>1,'l'=>1,'n'=>1,'r'=>1,'s'=>1,'t'=>1,'d'=>2,'g'=>2,'b'=>3,'c'=>3,'m'=>3,
+      'p'=>3,'f'=>4,'h'=>4,'v'=>4,'w'=>4,'y'=>4,'k'=>5,'j'=>8,'x'=>8,'q'=>10,'z'=>10}
+      new_array.each() do |letter|
+        score+=letter_hash.fetch(letter)
+      end
+      score
+  end
 end
--- spec/scrabble_score_spec.rb
+++ spec/scrabble_score_spec.rb
@@ -3,12 +3,12 @@ require('scrabble_score')
 
  describe('scrabble_score') do
   it("returns a scrabble score for a letter") do
-   expect(scrabble_score("a")).to(eq(1))
+   expect(("a").scrabble_score()).to(eq(1))
   end
   it("returns compiled scrabble score from word") do
-    expect(scrabble_score("muffin")).to(eq(14))
+    expect(("muffin").scrabble_score()).to(eq(14))
   end
   it("takes into account uppercase letters") do
-    expect(scrabble_score("Stalin")).to(eq(6))
+    expect(("Stalin").scrabble_score()).to(eq(6))
   end
 end
